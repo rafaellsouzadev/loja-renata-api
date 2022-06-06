360993620647b076cc5b6543c31febd9b685dde1
Rafael Souza <rafaellsouza.java@gmail.com>
2022-06-06 09:23:16 -0300

    inicialização do projeto

---
+++ src/main/java/com/rafael/lojarenata/LojaRenataApiApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.lojarenata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaRenataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaRenataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/lojarenata/LojaRenataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.lojarenata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaRenataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
